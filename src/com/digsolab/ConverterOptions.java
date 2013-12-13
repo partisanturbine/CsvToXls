13cbb63fd127759500d63f389cfda30722d4ac6b
cool-hazcker <daniil.migachev@gmail.com>
2013-12-13 12:48:41 +0400

    Added parseOptions method to ConverterOptions

--- src/com/digsolab/ConverterOptions.java
+++ src/com/digsolab/ConverterOptions.java
@@ -1,5 +1,29 @@
 package com.digsolab;
 
-public class ConverterOptions {
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
+public class ConverterOptions {
+    
+	private String delimeter = null;
+	private Format[] formats = null;
+	private CommandLine line = null;
+	
+    private void parseConverterOptions() {
+    	
+    }
+    
+    private void getCLIArgs(String[] args) throws ParseException {
+    	    Options options = new Options();
+    		CommandLineParser parser = new BasicParser();
+    		line = parser.parse(options, args);
+    }
+	
+	public void parseOptions(String[] args) throws ParseException {
+		getCLIArgs(args);
+		parseConverterOptions();
+	}
 }
