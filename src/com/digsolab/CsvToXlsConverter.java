9e16cb922bd65a6e80fac06d4bfebde991c522fb
cool-hazcker <daniil.migachev@gmail.com>
2013-12-13 12:07:10 +0000

    Added convertToXls method

--- src/com/digsolab/CsvToXlsConverter.java
+++ src/com/digsolab/CsvToXlsConverter.java
@@ -1,5 +1,23 @@
 package com.digsolab;
 
-public class CsvToXlsConverter {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class CsvToXlsConverter {
+	
+	private ConverterOptions options = null;
+	
+	public void convertToXls(String strSource, String strDestination)
+			throws FileNotFoundException, IOException, IllegalArgumentException {
+		File source = new File(strSource);
+		File destination = new File(strDestination);
+		if (!source.exists()) {
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
